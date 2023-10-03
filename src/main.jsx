5e7bf9d040d84ae99daa1af15625d4ec62758a99
vikram110703 <sainivks4321@gmail.com>
2023-10-03 23:03:29 +0530

    server changed

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import App from './App.jsx';
 import { ChakraProvider, theme } from "@chakra-ui/react"
 
 
-export const server = "http://localhost:5000/api/v1";
-// export const server = "https://hostel-project-backend.onrender.com/api/v1";
+// export const server = "http://localhost:5000/api/v1";
+export const server = "https://hostel-project-backend.onrender.com/api/v1";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
