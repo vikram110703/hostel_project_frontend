ac3dbd9651e74d75b01cb4ff713e9544b059f25f
vikram110703 <sainivks4321@gmail.com>
2023-10-06 20:02:12 +0530

    padding left fixed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -85,10 +85,10 @@ const Home = () => {
         <Box bgColor="blue.200" css={{ backgroundColor: "#C2D4E7" }} minH={"container.sm"}>
             <Stack maxW={"80%"} minH={["100vh", "container.sm"]} m={"auto"} p="0"
                 display={"flex"} flexDirection={"column"} justifyContent={"center"} alignItems={"center"}>
-                <Container bgColor={"whiteAlpha.700"} shadow={"lg"} borderRadius={"lg"} transition={"all 0.3s"} p={"4rem"} m={"2rem"} >
+                <Container bgColor={"whiteAlpha.700"} shadow={"lg"} borderRadius={"lg"} transition={"all 0.3s"} p={["2rem","4rem"]} m={["1.5rem","2rem"]} >
 
                     <form onSubmit={handleSubmit} >
-                        <Text fontSize={"2xl"} ml={"30%"} color={"blue.200"} fontWeight={"bold"} >Fill Your Details</Text>
+                        <Text fontSize={"2xl"} ml={["20%","30%"]} color={"blue.200"} fontWeight={"bold"} >Fill Your Details</Text>
                         <FormControl isRequired>
                             <FormLabel>Name</FormLabel>
                             <Input type='text' borderColor={"blue.200"} name="name" value={formData.name} onChange={handleChange} />
@@ -133,7 +133,7 @@ const Home = () => {
                             <FormLabel>Room No</FormLabel>
                             <Input type='text' borderColor={"blue.200"} name="roomNo" value={formData.roomNo} onChange={handleChange} />
                         </FormControl>
-                        <Button type="submit" bgColor={"blue.200"} mt={"5"} ml={"35%"} >Submit</Button>
+                        <Button type="submit" bgColor={"blue.200"} mt={"5"} ml={["30%","35%"]} >Submit</Button>
                     </form>
                 </Container>
 
